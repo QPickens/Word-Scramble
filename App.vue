9fa291a76c83264a707373c65f053ec7cc4af337
QPickens <147748108+QPickens@users.noreply.github.com>
2024-04-04 20:10:01 -0500

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,26 @@
+<template>
+<HeaderNavBar />
+<MainContainer />
+<FooterBar />
+</template>
+
+<script>
+import HeaderNavBar from './components/HeaderNavBar.vue';
+import FooterBar from './components/FooterBar.vue';
+import MainContainer from './components/MainContainer.vue';
+export default {
+  name: 'App',
+  components: { 
+    HeaderNavBar, 
+    FooterBar, 
+    MainContainer 
+  },
+};
+</script>
+
+<style>
+header, 
+footer{
+  background-color: #3f7cad;
+}
+</style>
